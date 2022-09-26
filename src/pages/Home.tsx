7e2a931586a6d1574e022058439167e0b954d424
gykim <52883505+namiein@users.noreply.github.com>
2022-09-26 12:47:24 +0000

    feat : 홈 화면에 단어가 추가된 다음 다른 버튼이 노출되도록 로직 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,25 @@
 import { Link } from 'react-router-dom';
 
 export default function Home() {
+    const voca = localStorage.getItem('words') ? JSON.parse(localStorage.getItem('words') || '') : [];
+
     return (
         <main className="w-full h-screen flex items-center justify-center flex-col">
-            <h1 className="text-3xl font-bold underline mb-2.5">일일 단어장</h1>
-            <div className="w-[250px] flex justify-between items-center">
+            <h1 className="text-5xl font-bold underline tracking-tighter mb-10">My Voca</h1>
+            <div className={voca.length ? 'w-[300px] flex justify-between items-center' : 'w-[300px] flex justify-center items-center'}>
                 <Link className="border border-black rounded-md p-2.5 bg-black text-white font-bold hover:animate-pulse" to="/add">
                     단어 추가하기
                 </Link>
-                <Link className="border border-black rounded-md p-2.5 bg-black text-white font-bold hover:animate-pulse" to="/practice">
-                    단어 외우기
-                </Link>
+                {!!voca.length && (
+                    <>
+                        <Link className="border border-black rounded-md p-2.5 bg-black text-white font-bold hover:animate-pulse" to="/memorize">
+                            단어 외우기
+                        </Link>
+                        <Link className="border border-black rounded-md p-2.5 bg-black text-white font-bold hover:animate-pulse" to="/practice">
+                            시험 보기
+                        </Link>
+                    </>
+                )}
             </div>
         </main>
     );
