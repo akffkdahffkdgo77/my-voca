54a2d3dba61bc5cfef2881d89240095679098895
gykim <52883505+namiein@users.noreply.github.com>
2022-09-30 13:25:34 +0000

    mod : setIndex 부분 수정

--- src/pages/Memorize.tsx
+++ src/pages/Memorize.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 
 interface IVocaList {
     word: string;
@@ -22,8 +22,10 @@ function randomize(data: IVocaList[]) {
 }
 
 export default function Memorize() {
-    const voca = localStorage.getItem('words') ? JSON.parse(localStorage.getItem('words') || '') : [];
-    const dataset = voca.length && randomize(voca);
+    const dataset = useMemo(() => {
+        const voca = localStorage.getItem('words') ? JSON.parse(localStorage.getItem('words') || '') : [];
+        return randomize(voca);
+    }, []);
 
     const [index, setIndex] = useState(0);
 
@@ -31,10 +33,10 @@ export default function Memorize() {
         <div className="w-full h-screen flex items-center justify-center flex-col">
             <div className="border border-black rounded-md p-5 w-[400px] h-[300px] flex items-center justify-center text-[24px] font-medium">{dataset[index].word}</div>
             <div className="w-[400px] mt-2.5 flex justify-end items-center gap-x-[5px]">
-                <button className="bg-black rounded-md py-[5px] px-2.5 text-white text-[12px] font-bold" type="button" onClick={() => setIndex((prev) => prev - 1)}>
+                <button className="bg-black rounded-md py-[5px] px-2.5 text-white text-[12px] font-bold" type="button" onClick={() => setIndex((prev) => (prev > 0 ? prev - 1 : dataset.length - 1))}>
                     이전
                 </button>
-                <button className="bg-black rounded-md py-[5px] px-2.5 text-white text-[12px] font-bold" type="button" onClick={() => setIndex((prev) => prev + 1)}>
+                <button className="bg-black rounded-md py-[5px] px-2.5 text-white text-[12px] font-bold" type="button" onClick={() => setIndex((prev) => (prev < dataset.length - 1 ? prev + 1 : 0))}>
                     다음
                 </button>
             </div>
