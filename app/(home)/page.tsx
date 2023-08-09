b7657f96f0db6a442e4fbeeee8266259f8b76404
akffkdahffkdgo77 <52883505+akffkdahffkdgo77@users.noreply.github.com>
2023-08-09 20:53:56 +0900

    mod : home 화면 버튼 수정

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,28 +1,13 @@
 'use client';
 
-import { useEffect, useState } from 'react';
-
 import LinkButton from './link-button';
 
-import { getWords } from '@utils/localStorage';
-
 export default function Home() {
-    const [wordList, setWordList] = useState([]);
-
-    useEffect(() => {
-        setWordList(getWords());
-    }, []);
-
     return (
         <div className="flex w-full items-center justify-center [height:calc(100vh-100px)]">
-            <div className={wordList.length ? 'flex w-[300px] items-center justify-between' : 'flex w-[300px] items-center justify-center'}>
-                <LinkButton path="/add" text="단어 추가하기" />
-                {Boolean(wordList.length) && (
-                    <>
-                        <LinkButton path="/memorize" text="단어 외우기" />
-                        <LinkButton path="/practice" text="시험 보기" />
-                    </>
-                )}
+            <div className="flex items-center justify-between gap-5">
+                <LinkButton path="/add" text="NEW" />
+                <LinkButton path="/list" text="EXISTING" />
             </div>
         </div>
     );
