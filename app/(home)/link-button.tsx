8f19eb1de360056228c039f32a3f122088f9ea4f
akffkdahffkdgo77 <52883505+akffkdahffkdgo77@users.noreply.github.com>
2023-08-12 23:08:22 +0900

    mod : link button css 수정

--- app/(home)/link-button.tsx
+++ app/(home)/link-button.tsx
@@ -11,7 +11,7 @@ export default function LinkButton({ text, path }: LinkButtonPropsType) {
     return (
         <CustomizedLink
             href={path}
-            className="h-40 w-40 rounded-md  bg-slate-900 text-center text-2xl font-bold leading-[160px] text-slate-50 hover:animate-pulse dark:border-0 dark:bg-slate-300 dark:text-slate-900 max-sm:h-32 max-sm:w-32 max-sm:text-lg max-sm:leading-[128px]"
+            className="h-40 w-40 rounded-md bg-slate-900 text-center text-2xl font-bold leading-[160px] text-slate-50 hover:animate-pulse dark:border-0 dark:bg-slate-50 dark:text-slate-900 max-sm:h-32 max-sm:w-32 max-sm:text-lg max-sm:leading-[128px]"
         >
             {text}
         </CustomizedLink>
