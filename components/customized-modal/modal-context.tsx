3c37302c4e5df23dac6364a0cc5b67e23ee5d9a3
akffkdahffkdgo77 <52883505+akffkdahffkdgo77@users.noreply.github.com>
2023-09-03 19:02:27 +0900

    mod : modal 코드 정리

--- components/customized-modal/modal-context.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import { createContext } from 'react';
-
-import type { ModalContextType } from './types';
-
-const ModalContext = createContext<ModalContextType | undefined>(undefined);
-
-export default ModalContext;
