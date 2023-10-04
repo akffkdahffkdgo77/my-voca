13f171da3ce59b45e67d4aad6b75b6eae8f9c469
akffkdahffkdgo77 <52883505+akffkdahffkdgo77@users.noreply.github.com>
2023-10-04 22:26:44 +0900

    chore : 공통 컴포넌트 planning

--- src/components/Customized/Button.tsx
+++ src/components/Customized/Button.tsx
@@ -2,6 +2,22 @@ type ButtonType = React.ButtonHTMLAttributes<HTMLButtonElement> & {
     children: React.ReactNode;
 };
 
+/**
+ *  TODO LIST
+ *  - shape : rounded, square
+ *  - variant : outlined, contained, text
+ *  - size : mini, small, medium, large
+ *  - theme : rust, buttered-rum, christi, blue-chill, blue-gem, jazzberry-jam
+ *  - customizable properties:
+ *      - border-radius
+ *      - background-color
+ *      - dimension (width & height)
+ *  - state:
+ *      - disabled
+ *      - focus
+ *      - hover
+ *      - active
+ */
 export default function CustomizedButton({ children, type = 'button', onClick, ...props }: ButtonType) {
     return (
         <button
--- src/components/Customized/Typography.tsx
+++ src/components/Customized/Typography.tsx
@@ -7,6 +7,16 @@ type TypographyType = Props & {
     children: React.ReactNode;
 };
 
+/**
+ *  TODO LIST
+ *  - variant : h1 ~ h6, b24 ~ b12, c11 ~ c8
+ *  - customizable properties:
+ *      - font-size
+ *      - font-weight
+ *      - text-align
+ *      - color
+ *      - gutterBottom
+ */
 export default function CustomizedTypography({ component, children, ...rest }: TypographyType) {
     return React.createElement(component, { ...rest }, children);
 }
