1153caafbcf17e63f71e35cf76dca8c689f27e77
akffkdahffkdgo77 <52883505+akffkdahffkdgo77@users.noreply.github.com>
2023-10-20 14:12:26 +0900

    chore : pages 정리

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,7 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 
 import { ExclamationTriangleIcon } from '@heroicons/react/24/outline';
-import { CustomizedButton, CustomizedTypography } from 'components';
+import { Button, Typography } from 'components';
 
 export default function NotFound() {
     const navigate = useNavigate();
@@ -10,19 +10,19 @@ export default function NotFound() {
         <div className="fixed bottom-0 left-0 right-0 top-0 z-50 min-h-screen bg-slate-50 dark:bg-slate-900">
             <div className="flex h-full flex-col items-center justify-center">
                 <ExclamationTriangleIcon className="h-[200px] w-[200px] text-red-500" />
-                <CustomizedTypography component="h2" className="mb-10 text-6xl text-slate-900 dark:text-slate-50">
+                <Typography component="h2" className="mb-10 text-6xl text-slate-900 dark:text-slate-50">
                     404
-                </CustomizedTypography>
+                </Typography>
                 <div className="flex gap-5">
-                    <CustomizedButton
+                    <Button
                         onClick={() => navigate('/', { replace: true })}
                         className="rounded border border-slate-900 bg-slate-900 p-2.5 text-slate-50 hover:opacity-70 dark:border-slate-900 dark:bg-slate-50 dark:text-slate-900"
                     >
                         홈 페이지로 돌아가기
-                    </CustomizedButton>
-                    <CustomizedButton onClick={() => navigate(-1)} className="rounded border border-slate-900 p-2.5 text-slate-900 hover:opacity-70 dark:border-slate-50 dark:text-slate-50">
+                    </Button>
+                    <Button onClick={() => navigate(-1)} className="rounded border border-slate-900 p-2.5 text-slate-900 hover:opacity-70 dark:border-slate-50 dark:text-slate-50">
                         이전 페이지로 돌아가기
-                    </CustomizedButton>
+                    </Button>
                 </div>
             </div>
         </div>
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,45 +1,18 @@
 import { createBrowserRouter } from 'react-router-dom';
 
-import Layout from 'layout';
-import Add from 'pages/Add';
+import { Layout } from 'layout';
 import Home from 'pages/Home';
-import { List, ListDetail } from 'pages/List';
-import Memorize from 'pages/Memorize';
-import Practice from 'pages/Practice';
+import NotFound from 'pages/NotFound';
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <Layout />,
+        errorElement: <NotFound />,
         children: [
             {
                 path: '',
                 element: <Home />
-            },
-            {
-                path: 'add',
-                element: <Add />
-            },
-            {
-                path: 'list',
-                children: [
-                    {
-                        path: '',
-                        element: <List />
-                    },
-                    {
-                        path: ':id',
-                        element: <ListDetail />
-                    }
-                ]
-            },
-            {
-                path: 'memorize/:id',
-                element: <Memorize />
-            },
-            {
-                path: 'practice/:id',
-                element: <Practice />
             }
         ]
     }
