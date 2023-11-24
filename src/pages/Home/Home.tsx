a94f48c31a4d127f2c1f7796bb70b4c4867083f3
akffkdahffkdgo77 <52883505+akffkdahffkdgo77@users.noreply.github.com>
2023-11-24 12:58:15 +0900

    feat : 홈 화면 개선

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,71 @@
-import LinkButton from './LinkButton';
+import { useState } from 'react';
+
+import styled from '@emotion/styled';
+import { Button, CustomizedColorPicker, CustomizedDatePicker, CustomizedLabelButton, CustomizedMobileTextInput, CustomizedSelect, CustomizedTextInput, Input } from 'components';
+import tw from 'twin.macro';
+
+import { useMobile, useTheme } from 'hooks';
+import { StyleThemes, ThemeType, getLightBackgroundColor } from 'utils/theme';
+
+const TwContainer = styled.div(({ theme }: ThemeType) => [tw`h-full overflow-y-auto w-full py-10`, theme && getLightBackgroundColor(theme)]);
+
+const STATUS_OPTIONS = [
+    { label: 'TODO', value: 'todo' },
+    { label: 'PROGRESS', value: 'progress' },
+    { label: 'DONE', value: 'done' }
+];
 
 export default function Home() {
+    const { theme, handleClick } = useTheme();
+    const isMobile = useMobile();
+    const [isDouble, setIsDouble] = useState(false);
+    const [isDisabled, setIsDisabled] = useState(true);
+
     return (
-        <div className="flex w-full items-center justify-center [height:calc(100vh-100px)]">
-            <div className="flex w-full items-center justify-evenly">
-                <LinkButton path="/add" text="NEW" />
-                <LinkButton path="/list" text="EXISTING" />
-            </div>
-        </div>
+        <>
+            <header className="fixed left-0 right-0 top-0 z-10 h-14 p-3 text-right backdrop-blur-sm">
+                <div className="m-auto max-w-7xl space-x-2.5">
+                    {!isDisabled && (
+                        <Button variant="outlined" shape="square" size="medium" theme={StyleThemes.Gray} twStyle={tw`bg-white`} onClick={() => setIsDisabled((prev) => !prev)}>
+                            저장
+                        </Button>
+                    )}
+                    <Button variant="outlined" shape="square" size="medium" theme={StyleThemes.Gray} twStyle={tw`bg-white`} onClick={() => setIsDisabled((prev) => !prev)}>
+                        {isDisabled ? '수정' : '공부'}
+                    </Button>
+                </div>
+            </header>
+            <TwContainer theme={theme}>
+                <div className="mx-auto max-w-5xl px-5 pt-6">
+                    <div className="mb-5 flex items-center justify-center">
+                        <Input
+                            disabled={isDisabled}
+                            hiddenText="단어장 이름"
+                            type="text"
+                            variant="text"
+                            theme={theme}
+                            maxLength={20}
+                            placeholder="단어장 이름을 입력해 주세요"
+                            twStyle={tw`bg-inherit text-h1 h-full px-0 font-nanumpenscript`}
+                        />
+                        <CustomizedDatePicker theme={theme} />
+                    </div>
+                    <div className="mb-5 flex items-end gap-2.5">
+                        {!isMobile && <CustomizedLabelButton theme={theme} buttonText={isDouble ? '2x' : '1x'} onClick={() => setIsDouble((prev) => !prev)} />}
+                        <CustomizedSelect caption="Status" theme={theme} value="todo" options={STATUS_OPTIONS} />
+                    </div>
+                    {Array.from({ length: 10 }).map((_, index) =>
+                        isMobile ? (
+                            <CustomizedMobileTextInput key={index} isDisabled={isDisabled} theme={theme} />
+                        ) : (
+                            <CustomizedTextInput key={index} isDisabled={isDisabled} isDouble={isDouble} theme={theme} />
+                        )
+                    )}
+                </div>
+                <aside className="fixed right-10 top-1/2 hidden -translate-y-1/2 flex-col gap-y-2.5 rounded-full bg-white p-1 shadow-md desktop:flex">
+                    <CustomizedColorPicker onClick={handleClick} />
+                </aside>
+            </TwContainer>
+        </>
     );
 }
