b5dba83019a583dcec6111afffcb4d9b9ebb5578
akffkdahffkdgo77 <52883505+akffkdahffkdgo77@users.noreply.github.com>
2023-11-24 12:50:40 +0900

    rm : Error Boundary 제거

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,10 @@ import ReactDOM from 'react-dom/client';
 import 'styles/index.css';
 
 import App from 'app/App';
-import ErrorBoundary from 'app/ErrorBoundary';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     <React.StrictMode>
-        <ErrorBoundary>
-            <App />
-        </ErrorBoundary>
+        <App />
     </React.StrictMode>
 );
