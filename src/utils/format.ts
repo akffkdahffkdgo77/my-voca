6bbce04c396f4510b6ff83eb55f3b0dc87b257d0
akffkdahffkdgo77 <52883505+akffkdahffkdgo77@users.noreply.github.com>
2023-12-01 22:11:37 +0900

    mod : dayjs로 format

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,1 +1,3 @@
-export const formatDate = (date: string | number) => new Date(date).toLocaleDateString('ko-KR', { year: 'numeric', month: '2-digit', day: '2-digit' });
+import dayjs from 'dayjs';
+
+export const formatDate = (date: string | number | Date, format: string) => dayjs(date).format(format);
