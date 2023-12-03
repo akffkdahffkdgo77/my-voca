8a6a21753495cc82e7d0f904ce08e50b6f110cd2
akffkdahffkdgo77 <52883505+akffkdahffkdgo77@users.noreply.github.com>
2023-12-03 17:31:36 +0900

    mod : Loader 분리

--- src/components/Customized/Loader.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loader() {
-    return (
-        <div className="flex min-h-screen w-full items-center justify-center bg-white/30">
-            <div className="h-100pxr w-100pxr animate-rotate rounded-full border-8 border-gray-900 border-b-transparent" />
-        </div>
-    );
-}
