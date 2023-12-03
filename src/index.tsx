47b5b0064f9bc280971b2d61e1025b4918de2534
akffkdahffkdgo77 <52883505+akffkdahffkdgo77@users.noreply.github.com>
2023-12-03 15:28:13 +0900

    feat : react-query 설정

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom/client';
 
 import 'styles/index.css';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import App from 'app/App';
+
+import { worker } from 'mocks/browser';
+
+if (process.env.NODE_ENV === 'development') {
+    worker.start();
+}
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     <React.StrictMode>
-        <App />
+        <QueryClientProvider client={queryClient}>
+            <App />
+        </QueryClientProvider>
     </React.StrictMode>
 );
---
+++ src/utils/queryKeys.ts
@@ -0,0 +1,8 @@
+// Reference : https://tkdodo.eu/blog/effective-react-query-keys
+export const wordKeys = {
+    all: ['word'] as const,
+    lists: () => [...wordKeys.all, 'list'] as const,
+    list: (filters: string) => [...wordKeys.lists(), { filters }] as const,
+    details: () => [...wordKeys.all, 'detail'] as const,
+    detail: (id: number) => [...wordKeys.details(), id] as const
+};
