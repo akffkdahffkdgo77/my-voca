8d1d8609f4c188a0de51aac124fae10c0a0a3e8f
akffkdahffkdgo77 <52883505+akffkdahffkdgo77@users.noreply.github.com>
2023-12-07 19:50:10 +0900

    mod : dayjs 한국어 지원

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,3 +1,7 @@
 import dayjs from 'dayjs';
 
+require('dayjs/locale/ko');
+
+dayjs.locale('ko');
+
 export const formatDate = (date: string | number | Date, format: string) => dayjs(date).format(format);
