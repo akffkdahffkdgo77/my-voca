5f6f47f44bdc64366e30e3a84473f16b323a2ffc
akffkdahffkdgo77 <52883505+akffkdahffkdgo77@users.noreply.github.com>
2023-12-22 20:20:31 +0900

    mod : type 수정

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,18 +1,21 @@
 import { getLocalStorage, setLocalStorage } from './localStorage';
 
-export type WordsType = {
-    wordIdx: number;
-    failCount: number;
-    successCount: number;
+export type WordType = {
+    wordIdx: string;
     word: string;
     definition: string;
+    count: number;
+    isHighlighted: false;
+    isMemorized: false;
 };
 
 export type DataType = {
-    idx: number;
-    title: string;
-    createdAt: number;
-    words: WordsType[];
+    wordListIdx: string;
+    wordListName: string;
+    wordListDate: number;
+    status: string;
+    category: string;
+    words: WordType[];
 };
 
 export type DataListType = DataType[];
@@ -23,7 +26,7 @@ export function getWords() {
 
 export function getWord(idx: string) {
     const wordList: DataListType = getWords();
-    return wordList.filter((word) => word.idx.toString() === idx)?.[0];
+    return wordList.filter((word) => word.wordListIdx.toString() === idx)?.[0];
 }
 
 export function addWord(newWord: DataType) {
