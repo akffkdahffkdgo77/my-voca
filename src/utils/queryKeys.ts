2a75d6bbcb14d9ac2ed33f9d4d432b2c6b14c779
akffkdahffkdgo77 <52883505+akffkdahffkdgo77@users.noreply.github.com>
2023-12-23 19:29:36 +0900

    rm : remove unnecessary file

--- src/utils/queryKeys.ts
+++
@@ -1,8 +0,0 @@
-// Reference : https://tkdodo.eu/blog/effective-react-query-keys
-export const wordKeys = {
-    all: ['word'] as const,
-    lists: () => [...wordKeys.all, 'list'] as const,
-    list: (filters: string) => [...wordKeys.lists(), { filters }] as const,
-    details: () => [...wordKeys.all, 'detail'] as const,
-    detail: (id: string) => [...wordKeys.details(), id] as const
-};
