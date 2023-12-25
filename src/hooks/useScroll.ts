8e16e7db99ee435ef47d24e2fe567dee0b799937
akffkdahffkdgo77 <52883505+akffkdahffkdgo77@users.noreply.github.com>
2023-12-25 16:28:20 +0900

    mod : scroll이 0이면 버튼이 노출되지 않도록

--- src/hooks/useScroll.ts
+++ src/hooks/useScroll.ts
@@ -13,6 +13,12 @@ const useScroll = () => {
         const previousScrollPosition = previousScroll.current;
         const currentScrollPosition = window.scrollY;
 
+        if (currentScrollPosition <= 0) {
+            setIsVisible(false);
+            previousScroll.current = currentScrollPosition;
+            return;
+        }
+
         // 헤더 높이 이상으로 스크롤 이동했을 경우에만 헤더 미노출
         // 현재 스크롤 위치과 직전 스크롤 위치가 다를 경우 미노출
         if (
