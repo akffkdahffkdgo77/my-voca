0c3a055cf9f785c68462ca098b8888d37fa418be
akffkdahffkdgo77 <52883505+akffkdahffkdgo77@users.noreply.github.com>
2023-12-25 16:29:01 +0900

    mod : provider 위치 변경

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 
 import 'styles/index.css';
 
+import { ModalProvider, ToastProvider } from 'components';
+
 import App from 'app/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     <React.StrictMode>
-        <App />
+        <ModalProvider>
+            <ToastProvider position="bottom-center">
+                <App />
+            </ToastProvider>
+        </ModalProvider>
     </React.StrictMode>
 );
