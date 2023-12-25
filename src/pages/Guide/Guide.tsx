13515ead05aacfc01dcd8aebd29a20beaa726303
akffkdahffkdgo77 <52883505+akffkdahffkdgo77@users.noreply.github.com>
2023-12-25 16:54:07 +0900

    mod : background 수정

--- src/pages/Guide/Guide.tsx
+++ src/pages/Guide/Guide.tsx
@@ -24,7 +24,7 @@ export default function Guide() {
     };
 
     return (
-        <div className="min-w-1512pxr bg-gray-100/50 py-16">
+        <div className="min-w-1512pxr bg-gray-100/50 py-16 bg-grid">
             <header className="flex-end fixed left-0 right-0 top-0 z-10 flex h-16 items-center bg-gray-100/50 px-10 shadow-md backdrop-blur-sm">
                 <nav className="ml-auto w-auto">
                     <ul className="flex items-center gap-x-2.5">
