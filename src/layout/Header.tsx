0d287b57dfb94af9fa00d464b1692ed2195b1dcb
akffkdahffkdgo77 <52883505+akffkdahffkdgo77@users.noreply.github.com>
2023-12-27 15:15:42 +0900

    mod : header에 백업 페이지로 가는 버튼 추가

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,12 +1,14 @@
-import { useEffect, useRef, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 import styled from '@emotion/styled';
-import { Typography, useModal } from 'components';
-import tw from 'twin.macro';
+import { DocumentArrowUpIcon } from '@heroicons/react/24/outline';
+import { Button, Typography, useModal, useToast } from 'components';
+import tw, { theme as twinTheme } from 'twin.macro';
 
 import dayjs from 'dayjs';
 
+import { getWords } from 'utils/data';
 import { StyleThemes, getBackgroundColor, getLightBorderColor, getTextColor } from 'utils/theme';
 
 interface Props {
@@ -17,7 +19,8 @@ const MILLISECONDS = 1000;
 const SECONDS = MILLISECONDS * 60;
 const MINUTES = SECONDS * 60;
 const HOURS = MINUTES * 24;
-const DUE_DATE = dayjs().add(1, 'day').set('hour', 0).set('minute', 0).set('second', 0);
+// const DUE_DATE = dayjs().add(1, 'day').set('hour', 0).set('minute', 0).set('second', 0);
+const DUE_DATE = dayjs('2023-12-27 15:15:00');
 
 const customStyle = {
     timeStyle: tw`px-2 h-8 w-36 leading-8`,
@@ -31,52 +34,88 @@ const TwTriangle = styled.div(({ theme }: Props) => [
 
 export default function Header({ theme }: Props) {
     const navigate = useNavigate();
+    const { pathname } = useLocation();
+    const isRegister = pathname === '/register';
 
     const [time, setTime] = useState('');
     const timerId = useRef<NodeJS.Timer>();
     const handleModal = useModal();
-
-    const countDown = () => {
-        const today = new Date();
-        const diff = DUE_DATE.diff(today);
-
-        if (diff < 0) {
-            handleModal({ message: '새로운 데이터를 등록해주세요!', confirmText: '등록하러 가기!' }).then(() => {
-                localStorage.clear();
-                navigate('/register', { replace: true });
-            });
-            setTime('00:00:00');
-            clearInterval(timerId.current);
-        } else {
-            const hours = Math.floor((diff % HOURS) / MINUTES);
-            const minutes = Math.floor((diff % MINUTES) / SECONDS);
-            const seconds = Math.floor((diff % SECONDS) / MILLISECONDS);
-            setTime(`${`${hours}`.padStart(2, '0')} : ${`${minutes}`.padStart(2, '0')} : ${`${seconds}`.padStart(2, '0')}`);
-        }
-    };
+    const { setMessage } = useToast();
 
     useEffect(() => {
-        timerId.current = setInterval(countDown, 1000);
+        timerId.current = setInterval(() => {
+            const today = new Date();
+            const diff = DUE_DATE.diff(today);
+
+            if (diff < 0) {
+                if (isRegister) {
+                    localStorage.clear();
+                    setMessage('모든 데이터가 초기화되었습니다.', { variant: 'info' });
+                } else {
+                    handleModal({
+                        message: '새로운 데이터를 등록해주세요!',
+                        confirmText: '등록하러 가기!'
+                    }).then(() => {
+                        localStorage.clear();
+                        navigate('/register', { replace: true });
+                    });
+                }
+                setTime('00:00:00');
+                clearInterval(timerId.current);
+            } else {
+                const hours = Math.floor((diff % HOURS) / MINUTES);
+                const minutes = Math.floor((diff % MINUTES) / SECONDS);
+                const seconds = Math.floor((diff % SECONDS) / MILLISECONDS);
+                setTime(`${`${hours}`.padStart(2, '0')} : ${`${minutes}`.padStart(2, '0')} : ${`${seconds}`.padStart(2, '0')}`);
+            }
+        }, 1000);
+
         return () => clearInterval(timerId.current);
+    }, [isRegister]);
+
+    const handleClick = useCallback(() => {
+        if (getWords().length === 0) {
+            setMessage('등록된 단어장이 없습니다.\n단어장을 먼저 등록해 주세요.', { variant: 'warning' });
+        } else {
+            navigate('/export');
+        }
     }, []);
 
     return (
-        <header className="fixed left-0 right-0 top-0 z-10 h-14 p-3 text-right backdrop-blur-sm">
-            <div className="m-auto flex max-w-7xl items-center justify-end gap-x-2.5">
-                {time && (
+        <header className="fixed left-0 right-0 top-0 z-10 h-14 p-3 text-right shadow-md backdrop-blur-sm">
+            {time && (
+                <div className="m-auto flex max-w-7xl items-center justify-end gap-x-2.5">
                     <div className="group relative">
                         <Typography variant="h4" fontFamily="nanumpenscript" fontWeight="700" align="center" twStyle={{ ...customStyle.timeStyle, ...getTextColor(theme) }}>
                             {time}
                         </Typography>
                         <TwTriangle theme={theme} />
                         <Typography variant="b14" fontWeight="600" twStyle={{ ...customStyle.tooltipStyle, ...getBackgroundColor(theme) }}>
-                            테스트 종료까지 남은 시간입니다.
-                            <br />
-                            종료 후 새로운 단어를 등록해 주세요.
+                            {isRegister ? (
+                                <>
+                                    모든 테스트 종료까지 남은 시간입니다.
+                                    <br />
+                                    종료 후 모든 데이터가 초기화됩니다.
+                                    <br />
+                                    <br />
+                                    <Typography component="small" variant="b12" fontWeight="600" color={twinTheme`colors.red.500`}>
+                                        데이터를 백업하고 싶다면 <DocumentArrowUpIcon className="mb-1 inline h-5 w-5 text-gray-950" /> 아이콘을 클릭해 주세요.
+                                    </Typography>
+                                </>
+                            ) : (
+                                <>
+                                    테스트 종료까지 남은 시간입니다.
+                                    <br />
+                                    종료 후 새로운 단어를 등록해 주세요.
+                                </>
+                            )}
                         </Typography>
                     </div>
-                )}
-            </div>
+                    <Button variant="icon" width={32} height={32} backgroundColor="transparent" title="데이터 백업하기" onClick={handleClick}>
+                        <DocumentArrowUpIcon className="m-auto h-7 w-7 text-gray-950" />
+                    </Button>
+                </div>
+            )}
         </header>
     );
 }
